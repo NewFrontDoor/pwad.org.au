71c7cff2e7617e12a602cea1848b1735aebccbca
Jonathan Haines <jonno.haines@gmail.com>
2020-11-09 21:12:33 +1100

    fix: use lazy queries for search

--- src/components/search-box/pray-search-control.tsx
+++ src/components/search-box/pray-search-control.tsx
@@ -1,11 +1,10 @@
 import React, {FC, useCallback} from 'react';
-import PropTypes from 'prop-types';
 import {useRouter} from 'next/router';
 import isEmpty from 'lodash/isEmpty';
 import {Styled, Box, Flex, Button} from 'theme-ui';
 import {Formik, Form, Field} from 'formik';
 import {TextField} from '../form';
-import {usePrayerSearchQuery, PrayerSearchQueryVariables} from '../queries';
+import {usePrayerSearchLazyQuery, PrayerSearchQueryVariables} from '../queries';
 import Loading from '../loading';
 import ServerError from '../server-error';
 import SearchResult, {isSearchResult} from './search-result';
@@ -13,55 +12,6 @@ import SearchOccasionInput from './search-occasion-input';
 import SearchKeywordInput from './search-keyword-input';
 import initialSelectValue from './initial-select-value';
 import {prefetchOnePrayer} from '../../prefetch';
-
-type AdvancedSearchProps = {
-  search: PrayerSearchQueryVariables;
-};
-
-const AdvancedSearch: FC<AdvancedSearchProps> = ({search}) => {
-  const {loading, error, data, client} = usePrayerSearchQuery({
-    variables: search,
-    fetchPolicy: 'network-only'
-  });
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <ServerError error={error} />;
-  }
-
-  if (data?.prayerSearch?.length > 0) {
-    return (
-      <>
-        {data.prayerSearch.map((result) => {
-          if (isSearchResult(result)) {
-            return (
-              <SearchResult
-                {...result}
-                key={result._id}
-                prefetch={() =>
-                  prefetchOnePrayer(client, {
-                    id: result._id
-                  })
-                }
-              />
-            );
-          }
-
-          return null;
-        })}
-      </>
-    );
-  }
-
-  return <Styled.p variant="prose">No results found...</Styled.p>;
-};
-
-AdvancedSearch.propTypes = {
-  search: PropTypes.object.isRequired
-};
 
 type SearchFields = {
   title?: string;
@@ -75,42 +25,29 @@ type SearchFields = {
 
 const SearchBox: FC = () => {
   const router = useRouter();
+
+  const [search, {loading, error, data, client}] = usePrayerSearchLazyQuery();
+
   const handleSubmit = useCallback(
     ({occasion, keyword, title}: SearchFields) => {
-      const query: PrayerSearchQueryVariables = {title};
+      const variables: PrayerSearchQueryVariables = {title};
 
       if (occasion) {
-        query.occasion = occasion.value;
+        variables.occasion = occasion.value;
       }
 
       if (keyword) {
-        query.keyword = keyword.value;
+        variables.keyword = keyword.value;
       }
 
-      void router.push(
-        {
-          pathname: router.pathname,
-          query
-        },
-        router.pathname,
-        {shallow: true}
-      );
+      search({variables});
     },
-    [router]
+    [search]
   );
 
   const handleListAll = useCallback(() => {
-    void router.push(
-      {
-        pathname: router.pathname,
-        query: {
-          title: ''
-        }
-      },
-      router.pathname,
-      {shallow: true}
-    );
-  }, [router]);
+    search({variables: {title: ''}});
+  }, [search]);
 
   const showSearchResults = !isEmpty(router.query);
   let initialValues: SearchFields = {
@@ -164,7 +101,32 @@ const SearchBox: FC = () => {
           </Flex>
         </Form>
       </Formik>
-      {showSearchResults && <AdvancedSearch search={router.query} />}
+      {loading && <Loading />}
+      {error && <ServerError error={error} />}
+      {data?.prayerSearch?.length === 0 && (
+        <Styled.p variant="prose">No results found...</Styled.p>
+      )}
+      {data?.prayerSearch?.length > 0 && (
+        <>
+          {data.prayerSearch.map((result) => {
+            if (isSearchResult(result)) {
+              return (
+                <SearchResult
+                  {...result}
+                  key={result._id}
+                  prefetch={() =>
+                    prefetchOnePrayer(client, {
+                      id: result._id
+                    })
+                  }
+                />
+              );
+            }
+
+            return null;
+          })}
+        </>
+      )}
     </>
   );
 };
